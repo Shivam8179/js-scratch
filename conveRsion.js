bf57edd2c37d8798b3e2a5e927d689853ccfc63e
shivam gupta <shivamgupta1811207@gmail.com>
2025-07-22 10:16:39 +0530

    first commit

---
+++ conveRsion.js
@@ -0,0 +1,20 @@
+//let id = "123";
+//let newId = Number(id);
+//console.log(newId);
+//console.log(typeof newId);
+
+//data types 
+//let user ="shivam";
+//let age =18;
+//let gmail = "shivam@123gmail.com";
+//console.log(`user is ${user} and age is ${age} gmail is ${gmail}`);
+//number of data types
+//console.log(1+2+"3");
+
+//console.log(1+2+"2");
+
+//increment and decrement
+let x =3;
+let y = x--;
+console.log(x);
+console.log(y);
